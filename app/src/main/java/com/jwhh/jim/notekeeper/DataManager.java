228aaf0d02dc26702358dfce21450b1b48004181
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-08-14 16:20:36 +0100

    Querying Course_Info Table. closes #4

--- app/src/main/java/com/jwhh/jim/notekeeper/DataManager.java
+++ app/src/main/java/com/jwhh/jim/notekeeper/DataManager.java
@@ -1,5 +1,9 @@
 package com.jwhh.jim.notekeeper;
 
+import android.database.sqlite.SQLiteDatabase;
+
+import com.jwhh.jim.notekeeper.NoteKeeperDatabaseContract.CourseInfoEntry;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,10 +20,18 @@ public class DataManager {
     public static DataManager getInstance() {
         if(ourInstance == null) {
             ourInstance = new DataManager();
-            ourInstance.initializeCourses();
-            ourInstance.initializeExampleNotes();
+//            ourInstance.initializeCourses();
+//            ourInstance.initializeExampleNotes();
         }
         return ourInstance;
+    }
+
+    public static void loadFromDatabase(NoteKeeperOpenHelper dbHelper){
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String[] courseColumns = {
+                CourseInfoEntry.COLUMN_COURSE_ID,
+                CourseInfoEntry.COLUMN_COURSE_TITLE};
+        db.query(CourseInfoEntry.TABLE_NAME, courseColumns, null, null, null, null, null);
     }
 
     public String getCurrentUserName() {

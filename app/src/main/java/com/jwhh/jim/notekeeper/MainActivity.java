b92112839d60575e04f0a8c80453163530ebee29
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-08-25 22:43:24 +0100

    Start Service and Refactoring. closes #36

--- app/src/main/java/com/jwhh/jim/notekeeper/MainActivity.java
+++ app/src/main/java/com/jwhh/jim/notekeeper/MainActivity.java
@@ -219,7 +219,9 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void backupNotes() {
-        NoteBackup.doBackup(MainActivity.this, NoteBackup.ALL_COURSES);
+        Intent intent = new Intent(this, NoteBackupService.class);
+        intent.putExtra(NoteBackupService.EXTRA_COURSE_ID, NoteBackup.ALL_COURSES);
+        startService(intent);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")

888d8110a26d57335fa8ecc47f7db58477aca819
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-08-26 22:35:05 +0100

    Launching a Job work in the background. closes #42

--- app/src/main/java/com/jwhh/jim/notekeeper/NoteUploaderJobService.java
+++ app/src/main/java/com/jwhh/jim/notekeeper/NoteUploaderJobService.java
@@ -1,20 +1,44 @@
 package com.jwhh.jim.notekeeper;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.IBinder;
 
 public class NoteUploaderJobService extends JobService {
     public static final String EXTRA_DATA_URI = "com.jwhh.jim.notekeeper.extras.DATA_URI";
+    private NoteUploader mNoteUploader;
 
     public NoteUploaderJobService() {
     }
 
+    @SuppressLint("StaticFieldLeak")
     @Override
     public boolean onStartJob(JobParameters params) {
-        return false;
+        AsyncTask<JobParameters, Void, Void> task = new AsyncTask<JobParameters, Void, Void>() {
+            @Override
+            protected Void doInBackground(JobParameters... backgroundParameters) {
+                JobParameters jobParams = backgroundParameters[0];
+
+                String stringDataUri = jobParams.getExtras().getString(EXTRA_DATA_URI);
+                Uri dataUri = Uri.parse(stringDataUri);
+                mNoteUploader.doUpload(dataUri);
+
+                jobFinished(jobParams, false);
+
+                return null;
+            }
+        };
+
+        mNoteUploader = new NoteUploader(this);
+        task.execute(params);
+
+
+        return true;
     }
 
     @Override

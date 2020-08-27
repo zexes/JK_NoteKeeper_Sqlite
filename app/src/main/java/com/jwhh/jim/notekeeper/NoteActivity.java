48bf6a396a1290086079daea03f9cd2f8ccab902
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-08-27 22:09:16 +0100

    Passing Receiver Events to an Activity. closes #1

--- app/src/main/java/com/jwhh/jim/notekeeper/NoteActivity.java
+++ app/src/main/java/com/jwhh/jim/notekeeper/NoteActivity.java
@@ -1,5 +1,6 @@
 package com.jwhh.jim.notekeeper;
 
+import android.annotation.SuppressLint;
 import android.app.LoaderManager;
 import android.content.ContentUris;
 import android.content.ContentValues;
@@ -166,6 +167,7 @@ public class NoteActivity extends AppCompatActivity
         Log.d(TAG, "************** onPause **************");
     }
 
+    @SuppressLint("StaticFieldLeak")
     private void deleteNoteFromDatabase() {
         AsyncTask task = new AsyncTask() {
             @Override
@@ -227,6 +229,8 @@ public class NoteActivity extends AppCompatActivity
         mSpinnerCourses.setSelection(courseIndex);
         mTextNoteTitle.setText(noteTitle);
         mTextNoteText.setText(noteText);
+
+        CourseEventBroadcastHelper.sendEventBroadcast(this, courseId, "Editing Note");
     }
 
     private int getIndexOfCourseId(String courseId) {
@@ -259,6 +263,7 @@ public class NoteActivity extends AppCompatActivity
 
     }
 
+    @SuppressLint("StaticFieldLeak")
     private void createNewNote() {
         AsyncTask<ContentValues, Integer, Uri> task = new AsyncTask<ContentValues, Integer, Uri>() {
             private ProgressBar mProgressBar;
